d13398a72af337a76a996f0f5db89b2d0b966f66
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-11-05 13:13:01 +0200

    q4

---
+++ ForDemo/q3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER:");
+            string input = Console.ReadLine();
+            int count=0, sum = 0;
+
+            for (; input != "done"; count++)
+            {
+                sum += int.Parse(input);
+
+                Console.WriteLine("ENTER:");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(sum*1.0 /count);
+        }
+    }
+}
---
+++ ForDemo/q4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+
+            //opt1
+            bool isPrime = true;
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine($"{num} is prime!");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is not prime!");
+            }
+
+            //opt1.2
+            //bool isPrime = true;
+            //for (int i = 2; i < Math.Sqrt(num); i++)
+            //{
+            //    if (num % i == 0)
+            //    {
+            //        isPrime = false;
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine($"{num} is {(isPrime ? "" : "NOT")} prime!");
+
+
+            //opt2
+            //int i =2;
+            //for (; i <= Math.Sqrt(num); i++)
+            //{
+            //    if (num % i == 0)
+            //    {
+            //        Console.WriteLine($"{num} is NOT prime!");
+            //        break;
+            //    }
+            //}
+            //if (Math.Sqrt(num) <  i )
+            //{
+            //    Console.WriteLine($"{num} is prime!");
+            //}           
+        }
+    }
+}
+
