a03758817408494e2a1a153efd941941db47d0bf
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-11-05 12:40:15 +0200

    q1 solved

--- ForDemo/q1/Program.cs
+++ ForDemo/q1/Program.cs
@@ -10,6 +10,15 @@ namespace q1
     {
         static void Main(string[] args)
         {
+            Console.Write("ENTER A NUMBER: ");
+            int num = int.Parse(Console.ReadLine());
+            int fact = 1;
+
+            for (int i = 2; i <= num; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine("fact =  " + fact);
         }
     }
 }
