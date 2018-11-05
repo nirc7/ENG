f7fb193f03760e0e708ea8dfc660169227f86bb8
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-11-05 12:46:15 +0200

    added q2

---
+++ ForDemo/q2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (i%7 == 0 )
+                {
+                    Console.Write("BOOM\t");
+                    continue;
+                }
+                Console.Write(i + "\t");
+            }
+        }
+    }
+}
