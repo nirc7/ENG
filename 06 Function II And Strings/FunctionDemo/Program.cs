36ae82620670428171936ed1b3fbf5f4e1417ee7
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-11-06 13:03:02 +0200

    string added

---
+++ 06 Function II And Strings/FunctionDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=7, num2;
+            FuncOut(out num,out num2 );
+            Console.WriteLine(num);//9
+            Console.WriteLine(num2);//10
+        }
+
+        static void FuncOut(out int num, out int num2)
+        {
+            num = int.Parse( Console.ReadLine());
+
+            num++;//9
+            num2 = 10;
+        }
+    }
+}
---
+++ 06 Function II And Strings/StringDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "a v i cohen";
+            Console.WriteLine(name.Length);
+            string sub = name.Substring(2);
+            Console.WriteLine(sub);
+            string sub2 = name.Substring(2,5);
+            Console.WriteLine(sub2);
+
+        }
+    }
+}
