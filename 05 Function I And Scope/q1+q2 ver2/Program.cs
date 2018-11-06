b29ff9ae7272708c3773c86a380a5dd2e9003070
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-11-06 10:41:40 +0200

    q1+q2

---
+++ 05 Function I And Scope/q1+q2 ver2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q1_q2_ver2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            AllPrimes(num);
+            Console.WriteLine();
+        }
+
+        static void AllPrimes(int num)
+        {
+            for (int i = 2; i <= num; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.Write(i + ", ");
+                }
+            }
+        }
+
+        static bool IsPrime(int num)
+        {
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
