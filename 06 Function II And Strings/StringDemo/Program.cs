e40f648bd025cee73e55d7991dba6a9307d7c54d
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-11-12 09:58:29 +0200

    findSeq

--- 06 Function II And Strings/StringDemo/Program.cs
+++ 06 Function II And Strings/StringDemo/Program.cs
@@ -20,15 +20,63 @@ namespace StringDemo
             char c = name[2];
             //name[2] = 'V';
             name = ChangeToUpper(name, 7);
-            Console.WriteLine(name);           
+            Console.WriteLine(name);
 
+            Console.WriteLine(CountLetter("1234434257", '8'));
+            Console.WriteLine(FindSubSequence("123345789", "123"));
+            Console.WriteLine(FindSubSequence2("123345789", "344"));
+        }
+
+        static bool FindSubSequence2(string sequence, string sub)
+        {
+            for (int i = 0; i <= sequence.Length - sub.Length; i++)
+            {
+                if (sequence.Substring(i, sub.Length) == sub)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static bool FindSubSequence(string sequence, string sub)
+        {
+            for (int i = 0; i <= sequence.Length - sub.Length; i++)
+            {
+                int j = 0;
+                for (; j < sub.Length; j++)
+                {
+                    if (sequence[i + j] != sub[j])
+                    {
+                        break;
+                    }
+                }
+                if (j == sub.Length)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static int CountLetter(string sentence, char letter)
+        {
+            int counter = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == letter)
+                {
+                    counter++;
+                }
+            }
+            return counter;
         }
 
         static string ChangeToUpper(string name, int index)
         {
-            name = name.Substring(0, index) + 
+            name = name.Substring(0, index) +
                     name[index].ToString().ToUpper()/*converted to string in order to use the ToUpper() function*/ +
-                    name.Substring(index+1);
+                    name.Substring(index + 1);
             return name;
         }
     }
