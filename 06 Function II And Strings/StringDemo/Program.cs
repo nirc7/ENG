11a28da5d581baffb984326c4a5c2dce3eb30f8c
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-11-12 09:03:00 +0200

    toUpper

--- 06 Function II And Strings/StringDemo/Program.cs
+++ 06 Function II And Strings/StringDemo/Program.cs
@@ -13,11 +13,23 @@ namespace StringDemo
 
             string name = "a v i cohen";
             Console.WriteLine(name.Length);
-            string sub = name.Substring(2);
+            string sub = name.Substring(6);
             Console.WriteLine(sub);
-            string sub2 = name.Substring(2,5);
+            string sub2 = name.Substring(2, 5);
             Console.WriteLine(sub2);
+            char c = name[2];
+            //name[2] = 'V';
+            name = ChangeToUpper(name, 7);
+            Console.WriteLine(name);           
 
+        }
+
+        static string ChangeToUpper(string name, int index)
+        {
+            name = name.Substring(0, index) + 
+                    name[index].ToString().ToUpper()/*converted to string in order to use the ToUpper() function*/ +
+                    name.Substring(index+1);
+            return name;
         }
     }
 }
