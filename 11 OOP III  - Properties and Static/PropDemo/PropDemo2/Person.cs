afba41f4bf2107fa631873807a3cb2023dbefa72
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-12-03 10:04:10 +0200

    PropStaticEnumConstDemo2

---
+++ 11 OOP III  - Properties and Static/PropDemo/PropDemo2/Person.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace PropDemo2
+{
+    public enum Course:byte {BIO, MATH, ENG=10, CS, REACT }//default int
+
+    public class Person
+    {
+        int id;
+        string name;
+        static int counter = 10;
+        const string SCHOOL = "Ruppin";
+        Course myCourse = Course.REACT;
+
+        public int Age { get; set; }
+
+        public Person()
+        {
+            Id = 7;
+            Name = "no";
+            counter++;
+        }
+
+        public static int Counter
+        {
+            get { return counter - 10; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (0 < value && value <= 100)
+                {
+                    id = value;
+                }
+                else
+                {
+                    Console.WriteLine("ERR ID!");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return name + ":)"; }
+            set
+            {
+                if (2 < value.Length)
+                {
+                    name = value;
+                }
+                else
+                {
+                    name = "errName";
+                    Console.WriteLine("ERR NAME!");
+                }
+            }
+        }
+
+        public Course MyCourse
+        {
+            get { return myCourse; }
+            set {
+                if (Course.BIO <= value &&  value <= Course.REACT )
+                {
+                    myCourse = value;
+                }
+                else
+                {
+                    Console.WriteLine("ERR MYCOURSE");
+                }
+            }
+        }
+
+        public void Print() // instance (not static)
+        {
+            Console.WriteLine($"{Id},{Name},{Counter},{SCHOOL},{MyCourse}");
+        }
+    }
+}
---
+++ 11 OOP III  - Properties and Static/PropDemo/PropDemo2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropDemo2
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Print();
+            p1.Name = "avi";
+            p1.Print();
+            Console.WriteLine(Person.Counter);
+
+            Course c1 = Course.CS;
+            Console.WriteLine(c1);
+            Console.WriteLine((int)c1);
+            Console.WriteLine((int)c1+1);
+            Console.WriteLine((Course)((byte)c1 + 1));
+            Console.WriteLine(  (Course)((int)c1 + 4)   );
+
+            Console.WriteLine((Course)1);
+
+            p1.MyCourse = Course.BIO;
+            p1.Print();
+
+            Console.WriteLine();
+            string[] courseNames = Enum.GetNames(typeof(Course));
+            byte[] courseNums = (byte[])Enum.GetValues(typeof(Course));
+
+            Console.WriteLine("choose one from the following list:");
+            for (int i = 0; i < courseNames.Length; i++)
+            { 
+                Console.WriteLine( courseNums[i] + " - " +  courseNames[i]);
+            }
+            Console.WriteLine();
+            string inputName =  Console.ReadLine();
+
+            if (Enum.IsDefined(typeof(Course), inputName.ToUpper()))
+            {
+                p1.MyCourse  = (Course)Enum.Parse(typeof(Course), inputName,true);
+            }
+            p1.Print();
+
+            int inputNum =  int.Parse(Console.ReadLine());
+            p1.MyCourse = (Course)inputNum;
+            p1.Print();
+
+        }
+    }
+
+}
