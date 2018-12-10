67033995e4cc204cc6154992bb590676448a3853
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-12-10 10:08:14 +0200

    bin_prt1

--- 12 Recursion/RecDemo/BinaryTree/Program.cs
+++ 12 Recursion/RecDemo/BinaryTree/Program.cs
@@ -14,6 +14,52 @@ namespace BinaryTree
             tree.Left = new Node(10);
             tree.Right = new Node(5);
             tree.Right.Left = new Node(100);
+
+            BinaryTree bt = new BinaryTree(50);
+            bt.head.Left = new Node(20);
+            bt.head.Left.Left = new Node(5);
+            bt.head.Left.Right = new Node(25);
+            bt.head.Right = new Node(70);
+            bt.PrintTree();
+
+        }
+    }
+
+    class BinaryTree
+    {
+        public Node head;
+
+
+        public BinaryTree()
+        {
+
+        }
+
+        public BinaryTree(int value)
+        {
+            head = new Node(value);
+        }
+
+        public bool IsEmpty() { return head == null; }
+
+        public void PrintTree()
+        {
+            Console.WriteLine();
+            Print(head);
+            Console.WriteLine();
+        }
+
+        private void Print(Node node)
+        {
+            if (node.Left != null)
+            {
+                Print(node.Left);
+            }
+            Console.Write(node.Value + ", ");
+            if (node.Right != null)
+            {
+                Print(node.Right);
+            }
         }
     }
 
