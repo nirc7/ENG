ce4e934c8d389af46de482ed583236237cebdd7b
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-12-10 09:30:32 +0200

    BinTree

---
+++ 12 Recursion/RecDemo/BinaryTree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node tree = new Node(20);
+            tree.Left = new Node(10);
+            tree.Right = new Node(5);
+            tree.Right.Left = new Node(100);
+        }
+    }
+
+    class Node
+    {
+        public int Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
