8806ec4e16fa0d194b6a9c3dcf3206df2a9406ed
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-12-11 13:03:02 +0200

    sort

--- 13 Algorithms And Computational Complexity/ConsoleApp1/ConsoleApp1/Program.cs
+++ 13 Algorithms And Computational Complexity/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,55 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int[] nums = new int[] { 3, 5, 7, 8, 11, 15, 18, 24, 25 };
+
+            int index = BinarySearch(nums, 7);
+            Console.WriteLine(index);
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine(  BinarySearch(nums, num));
+            }
+
+            Console.WriteLine(DateTime.Now.Millisecond);
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            Console.WriteLine(rnd.Next(0,100));
+
+            nums = new int[2000];
+            for (int i = 0; i < 2000; i++)
+            {
+                nums[i] = i;
+            }
+            
+            BinarySearch(nums, 80);
+
+            Console.WriteLine(Math.Log(1024,2));
+
+        }
+
+        private static int BinarySearch(int[] nums, int valueToSearch)
+        {
+            int counter = 0;
+            for (int start = 0, end = nums.Length - 1; start <= end;)
+            {
+                int middleIndex = (start + end) / 2;
+                counter++;
+                if (nums[middleIndex] == valueToSearch)
+                {
+                    Console.WriteLine("counter=" + counter);
+                    return middleIndex;
+                }
+                else if (valueToSearch < nums[middleIndex])
+                {
+                    end = middleIndex - 1;
+                }
+                else
+                {
+                    start = middleIndex + 1;
+                }
+            }
+            Console.WriteLine("counter"+ counter);
+            return -1;
         }
     }
 
---
+++ 13 Algorithms And Computational Complexity/ConsoleApp1/InsertionSort/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSortNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //swap XOR without temp
+            int num1 = 7;
+            int num2 = 5;
+            num2 ^= num1;
+            num1 = num2 ^ num1;
+            num2 ^= num1;
+
+            //swat +- without temp
+            num1 = 5;
+            num2 = 7;
+            num1 -= num2;//num1 = 2
+            num2 += num1;
+            num1 = num2 - num1;
+
+
+            int[] nums = new int[] { 3, 7, 2, 9, 1, 22, 5, 4 };
+            InsertionSort(nums);
+
+            Console.WriteLine();
+            foreach (var item in nums)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void InsertionSort(int[] nums)
+        {
+            int counter = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    counter++;
+                    if (nums[j] < nums[i])
+                    {
+                        //swap +- without temp
+                        nums[j] -= nums[i];
+                        nums[i] += nums[j];
+                        nums[j] = nums[i] - nums[j];
+                    }
+                }
+            }
+            Console.WriteLine("counter= " + counter);
+        }
+    }
+}
